77603646ed72443afd615c7e9a2b868fd90b9a9b
kyleimhoff <kyleimhoff@DESKTOP-22RNIN5.localdomain>
2020-11-11 21:06:17 -0500

    added validations to models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ApplicatonRecord 
     has_many :game_reviews
     has_many :users, through: :game_reviews
+    validates_presence_of :name, :esrb_rating, :description 
+    validates_uniqueness_of :name
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicatonRecord 
     has_many :game_reviews
     has_many :games, through: :game_reviews
+    validates :username, presence: true 
+    validates :username, uniqueiness: true
+    has_secure_password 
 end
