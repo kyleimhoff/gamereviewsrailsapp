d68f7c881bcacde7e49cd873fe5353c2ac7596ff
kyleimhoff <kyleimhoff@DESKTOP-22RNIN5.localdomain>
2020-11-11 21:26:04 -0500

    working on signup and login

---
+++ app/controllers/game_reviews_controller.rb
@@ -0,0 +1,3 @@
+class GameReviewsController < ApplicationController
+    
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+    def new 
+        @user = User.new
+    end
+    def create 
+        @user = User.new(user_params)
+        if @user.save 
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to new_user_path
+        end
+    end 
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def update 
+
+    end
+    
+    def destroy
+
+    end
+end
