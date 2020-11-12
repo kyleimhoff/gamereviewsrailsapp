e8f1b9b5c341172e77a62b6a88b17304c3dc0ccf
kyleimhoff <kyleimhoff@DESKTOP-22RNIN5.localdomain>
2020-11-11 21:35:34 -0500

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  root to: "games#index"
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  
+  resources :user do
+    resorces :game_reviews
+  end
+  resorces :game do
+    resources :game_reviews
+  end
+  resources :game_reviews
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
